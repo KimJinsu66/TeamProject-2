eefd28325fc77b0f60c8dec6c1570a658e8f84e4
KimJinsu66 <jinsu6688@gmail.com>
2020-04-03 17:08:15 +0900

    1. I changed name of hh.class to Speed.class 2 I made main method and RootWalk method 3. I added our name and explain

---
+++ Speed.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * included RobotWalk method in 
+ * 
+ * @author (2014671038 KIM JINSU, 2018315053 Teraoka Yuika) 
+ * @version (2020.04.05)
+ */
+public class Speed
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int speed = sc.nextInt();
+        System.out.println(speed);// just check 
+    }
+    
+    public int RobotWalk(){
+        int result = 0;
+        
+        return result; 
+    }
+}
