b2b33491902ddce79b45b8d10d3b3094b8092397
yuikateraoka <yuikasunmoon@gmail.com>
2020-04-03 22:22:08 +0900

    RobotWalk 메소드 작성했습니다.

--- Speed.java
+++ Speed.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
+import java.util.*;
 /**
  * included RobotWalk method in 
  * 
- * @author (2014671038 KIM JINSU, 2018315053 Teraoka Yuika) 
+ * @author (2014671038 Kim Jinsu, 2018315053 Teraoka Yuika) 
  * @version (2020.04.05)
  */
 public class Speed
-{
+{    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int speed = sc.nextInt();
-        System.out.println(speed);// just check 
-    }
-    
-    public int RobotWalk(){
-        int result = 0;
+        //System.out.println(speed);// just check 
+        String trafficRoute = sc.next();
+    }    
+
+    public double RobotWalk(int speed, String trafficLight){       
+        double result = 0; 
         
-        return result; 
+        if (trafficLight == "green"){
+            result = speed;
+        }else if (trafficLight == "yellow"){
+            result = speed * 1.2;                      
+        }else if(trafficLight == "red"){
+            result = speed * speed/1; 
+        }
+        return result;
     }
 }
