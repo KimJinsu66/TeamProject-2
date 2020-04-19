1e94684d2c701fda863d8370b150f7e5ec20ea02
yuikateraoka <yuikasunmoon@gmail.com>
2020-04-19 14:56:25 +0900

    test 실시 및 정리

--- RobotTest.java
+++ RobotTest.java
@@ -4,55 +4,34 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 /**
  * The test class RobotTest.
  *
- * @author  (2014671038 Kim Jinsu, 2018315053 Teraoka Yuika)
- * @version (2020.04.19)
+ * @author  (your name)
+ * @version (a version number or a date)
  */
 public class RobotTest
-{
-    /**
-     * Default constructor for test class RobotTest
-     */
-    public RobotTest()
-    {
-    }
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
-    }
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @After
-    public void tearDown()
-    {
-    }
-    
+{   
     @Test
-    public void RobotWalk_Test1()
+    public void RobotWalk_StatementCoverageTest()
     {
         Robot robot1 = new Robot();
-        assertEquals(10.0, robot1.getSpeed(), 0.1);
         robot1.RobotWalk(10.0, "yellow");
+        assertEquals(12.0, robot1.getSpeed(), 0.1);
         robot1.RobotWalk(10.0, "red");
-    }
-
-    @Test
-    public void RobotWalk_Test2()
-    {
-        Robot robot1 = new Robot();
         assertEquals(0.0, robot1.getSpeed(), 0.1);
-        robot1.RobotWalk(10.0, "yellow");
-        robot1.RobotWalk(10.0, "red");
+    }
+    @Test
+    public void RobotWalk_DecisionCoverageTest()
+    {
+        Robot robot2 = new Robot();
+        robot2.RobotWalk(10.0, "green");
+        assertEquals(0.0, robot2.getSpeed(), 0.1);
+        robot2.RobotWalk(10.0, "yellow");
+        assertEquals(12.0, robot2.getSpeed(), 0.1);
+        robot2.RobotWalk(10.0, "red");
+        assertEquals(0.0, robot2.getSpeed(), 0.1);
     }
 }
 
